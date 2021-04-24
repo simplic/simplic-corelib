a1f55ae32e27069feaa335aff3a61ba8c5a6de02
Benedikt Eggers <be@spiegelburg.net>
2021-04-24 17:11:09 +0200

    Replace BaseException with the core exception

--- src/Simplic.CoreLib/Exception/CoreException.cs
+++ src/Simplic.CoreLib/Exception/CoreException.cs
@@ -1,4 +1,4 @@
-﻿namespace Simplic.Exception
+﻿namespace Simplic
 {
     /// <summary>
     /// Simplic core exception, should be used when throwing an expected exception
@@ -19,16 +19,22 @@
         /// </summary>
         /// <param name="code">Unique error code</param>
         /// <param name="parameter">Additional parameter for the exception message</param>
-        public CoreException(string code, params string[] parameter)
+        public CoreException(string code, string id, params string[] parameter)
         {
             Code = code;
             Parameter = parameter;
+            Id = id;
         }
 
         /// <summary>
         /// Gets or sets the error-code
         /// </summary>
         public string Code { get; set; }
+
+        /// <summary>
+        /// Gets or sets the exception id. By default this should be guid
+        /// </summary>
+        public string Id { get; set; }
 
         /// <summary>
         /// Gets or sets the parameter for the error message
