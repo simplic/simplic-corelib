b2a8a14e57ac52aace0f4840c9fc2fee1b23dc11
Fabian Lüßenhop <Fabian.Luessenhop@simplic.biz>
2025-01-31 13:57:30 +0100

    Add RangeObservableCollection

---
+++ src/Simplic.CoreLib/Collections/Generic/RangeObservableCollection.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Simplic.Collections.Generic
+{
+    /// <summary>
+    /// Observable collection with faster addRange/removeRange methods.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class RangeObservableCollection<T> : ObservableCollection<T>
+    {
+        private bool _suppressNotification = false;
+
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            if (!_suppressNotification)
+                base.OnCollectionChanged(e);
+        }
+
+        /// <summary>
+        /// Adds a collection to the observable collection. Notifies surpesses notificaton while adding items.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public void AddRange(IEnumerable<T> list)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+
+            _suppressNotification = true;
+
+            foreach (T item in list)
+            {
+                Add(item);
+            }
+            _suppressNotification = false;
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+        /// <summary>
+        /// Removes a collection from the observable collection. Notifies surpesses notificaton while removing items.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public void RemoveRange(IEnumerable<T> list)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+
+            _suppressNotification = true;
+
+            foreach (T item in list)
+            {
+                Remove(item);
+            }
+            _suppressNotification = false;
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+    }
+}
